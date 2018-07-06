d88c162a149da38e83b9d5c180668007a1c46534
jenny274 <jennifer.ramananarivo@gmail.com>
2018-07-06 14:39:06 +0400

    Création des différents fichiers pour les utilisateurs. Création du modèle et du controller.

---
+++ controllers/utilisateursController.js
@@ -0,0 +1,33 @@
+//Controller pour les utilisateurs
+var mongoose = require('mongoose');
+
+var utilisateur = require("../models/utilisateurs");
+
+var utilisateursController = {};
+
+// Fonction  qui permet de renvoyer vers le formulaire d'inscription d'un utilisateur
+utilisateursController.create = function(req, res){
+    res.render("../views/utilisateurs/addUtilisateur");//, {error: req.session.error}
+}; 
+
+//fonction qui permet l'enregistrement d'un nouvel utilisateur
+utilisateursController.save = function(req, res){
+
+    var utilisateur = new utilisateur(req.body);
+    utilisateur.save(function(err){
+        if(err){
+        //req.body.error = 'Echec création utilisateur'
+            console.log(err);
+            res.render("../views/utilisateurs/addUtilisateur");
+
+        } else{
+            console.log("creation utilisateur OK");
+         //   req.session.success = 'Utilisateur créé et enregistré avec succès'
+            res.redirect("/personnes");
+    
+        } 
+    });
+};
+
+// module qui permet d'exporter le Controller utilisateur
+module.exports = utilisateursController;
---
+++ routes/utilisateurs.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+
+
+var utilisateur = require("../controllers/utilisateursController");
+
+// function requireLogin (req, res, next) {
+//     if (req.session && req.session.userId) {
+//         next();
+//     }else {
+//         var err = new Error('error 404');
+//         err.status = 401;
+//         res.redirect('/admin');
+//     }
+// };
+
+/* GET home page. */
+//router.get('/', requireLogin, personne.list );
+
+//cree une personne
+router.get("/creer", personne.create);
+
+//sauvegarder une personne
+router.post("/save", personne.save);
+
+// Supprimer une personne
+//router.get("/delete/:id", personne.delete);
+
+
+//editer une personne
+//router.post("/edit", personne.edit);
+
+
+// module permmettant d'exporter le router utilisateur
+module.exports = router;
