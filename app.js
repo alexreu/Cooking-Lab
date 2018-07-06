0002015bdf6438d7f6b81b9a3d764e069a1bcd9e
alexreu <alexxxx.974@gmail.com>
2018-07-06 13:40:22 +0400

    creation de la connexion à la bdd et ajout du module session

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
+var session = require('express-session');
 
-//connexion
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//connexion à la base de donnée
+mongoose.connect("mongodb://<alexandre>:<alexandre974>@ds127841.mlab.com:27841/cooking-lab")
+    .then(() => console.log('Connexion à la BDD OK'));
 
 var app = express();
 
@@ -21,9 +21,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+//utilisation des sessions
+app.use(session({
+    secret: 'Work Hard',
+    resave: true,
+    saveUninitialized: false
+}));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
