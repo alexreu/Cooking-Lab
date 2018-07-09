50624e84f7f368e6878d8c8a4bee6d2fa418fd71
Adama Diallo <adamadial58@me.com>
2018-07-09 10:56:20 +0400

    creation modelSchema reservations

---
+++ models/reservationsModel.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+
+// schema de données de la collection reservations
+var reservationsSchema = new mongoose.schema({
+    id_user : [{type: mongoose.Schema.ObjectId, ref: 'utilisateurs'}],
+    id_ateliers :  [{type: mongoose.Schema.ObjectId, ref: 'ateliers'}],
+});
+
+
+
+
+// module qui permet d'exporter le modèle du schéma de données de la collection reservations 
+module.exports = mongoose.model("reservations", reservationsSchema);
